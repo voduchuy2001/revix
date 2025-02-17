a6707315dcf4b1c6dfb600d23215ea0037bda28d
Vo Duc Huy <voduchuy2001@gmail.com>
2025-02-17 16:57:57 +0700

    update: nav bar

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,5 @@
+import logo from "@/assets/logo.png";
+
+export default function ApplicationLogo(props) {
+    return <img className="" src={logo} alt="Táo cười" />;
+}
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,7 +1,8 @@
+import ApplicationLogo from "@/Components/ApplicationLogo";
 import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
-import { usePage } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
@@ -17,7 +18,13 @@ export default function AuthenticatedLayout({ header, children }) {
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="flex h-16 justify-between">
                         <div className="flex">
-                            <div className="hidden space-x-8 sm:-my-px sm:flex">
+                            <div className="flex shrink-0 items-center">
+                                <Link href="/">
+                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                                </Link>
+                            </div>
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink
                                     href={route("dashboard")}
                                     active={route().current("dashboard")}
