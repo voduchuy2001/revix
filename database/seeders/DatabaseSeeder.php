2ca79d4dca043b5a8805cb7e7e6ec8ab3ac5236f
voduchuy2001 <voduchuy2001@gmail.com>
2025-02-18 17:16:16 +0700

    update: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Device;
-use App\Models\RepairTicket;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,28 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-          'name' => 'Võ Đức Huy',
-          'email' => 'voduchuy2001@gmail.com',
+        $this->call([
+            UserSeeder::class,
+            SettingSeeder::class
         ]);
-
-        $customers = User::factory()->count(10)->create([
-          'type' => 'customer',
-        ]);
-
-        $technicians = User::factory()->count(5)->create([
-            'type' => 'technician',
-        ]);
-
-        $devices = Device::factory()->count(30)->create();
-
-        $devices->each(function ($device) use ($customers, $technicians) {
-            RepairTicket::factory()->count(2)->create([
-                'customer_id' => $customers->random()->id,
-                'device_id' => $device->id,
-                'technician_id' => $technicians->random()->id,
-                'condition' => fake()->realText(191)
-            ]);
-        });
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserType;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Võ Đức Huy',
+            'email' => 'voduchuy2001@gmail.com',
+            'password' => 'admin123@',
+            'phone_number' => '0962785101',
+            'address' => 'An Hòa, Ninh Kiều, Cần Thơ',
+            'type' => UserType::USER->value,
+        ]);
+    }
+}
