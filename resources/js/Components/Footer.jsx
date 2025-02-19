8ca82c08cc8e54c2de510cf051c61157305f99a9
voduchuy2001 <voduchuy2001@gmail.com>
2025-02-19 09:58:28 +0700

    chores

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
 
   return (
     <footer className="py-4 text-gray-500 text-center w-full">
-      <a href="https://voduchuy.vercel.app" target="_blank">
-        &#169; {currentYear} Made by Vo Duc Huy ❤. All rights reserved
+      <a href="https://duydangdigital.com" target="_blank">
+        Copyright {currentYear} &#169; DUY DANG DIGITAL | Designed by Vo Duc Duy
       </a>
     </footer>
   )
