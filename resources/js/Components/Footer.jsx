e687753dcef3e526e7cc1e88b99273649dd9e22c
Vo Duc Huy <voduchuy2001@gmail.com>
2025-03-15 17:24:52 +0700

    chores

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -3,9 +3,7 @@ export default function Footer() {
 
   return (
     <footer className="py-4 text-gray-500 text-center w-full">
-      <a href="https://duydangdigital.com" target="_blank">
-        Copyright {currentYear} &#169; DUY DANG DIGITAL | Designed by Vo Duc Huy
-      </a>
+      <a href="#!">Copyright {currentYear} &#169; DUY DANG DIGITAL | Designed by Vo Duc Huy</a>
     </footer>
   )
 }
